72ab352c89939c3422b8eee21639911cb53308fa
Yohh <durandyohan@zaclys.net>
2023-03-02 17:04:00 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Wheel } from "react-custom-roulette";
+import { data } from "./data";
+import "./App.scss";
+import { useState } from "react";
 
 function App() {
+  const [spin, setSpin] = useState<boolean>(false);
+  const [random, setRandom] = useState<number>(0);
+
+  const handleSpin = () => {
+    setRandom(Math.floor(Math.random() * 3));
+    setSpin(true);
+    setTimeout(() => setSpin(false), 1000);
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <span className="App-title">c ki ki review ma pr??</span>
+      <button className="App-button" onClick={() => handleSpin()}>
+        tente ta chance
+      </button>
+      <Wheel mustStartSpinning={spin} prizeNumber={random} data={data} />
     </div>
   );
 }
---
+++ src/data.ts
@@ -0,0 +1,18 @@
+export const data = [
+  {
+    option: "Mathieu",
+    style: { backgroundColor: "green", textColor: "black" },
+  },
+  {
+    option: "Alexandre",
+    style: { backgroundColor: "yellow", textColor: "black" },
+  },
+  {
+    option: "Nicolas",
+    style: { backgroundColor: "red", textColor: "black" },
+  },
+  {
+    option: "Valentin",
+    style: { backgroundColor: "cyan", textColor: "black" },
+  },
+];
